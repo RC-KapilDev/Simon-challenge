f153ef222a32516b0ad7a006b700035c4c5913d4
kapildev <rckapildev8@gmail.com>
2024-01-26 10:41:10 -0800

    first commit

---
+++ Simon Game Challenge Starting Files/index.js
@@ -0,0 +1,79 @@
+var color = ['green','red','yellow','blue'];
+var level = 0;
+var gamePattern = [];
+var userPattern = [];
+var start = false;
+
+$(document).on('keydown',function(){
+    if(!start){
+        sequence();
+        $('#level-title').text('Level '+ level);
+        start = true;
+    }
+        
+    }
+);
+
+$('.btn').click(
+    function(){
+        var userChoosed = $(this).attr('id');
+        userPattern.push(userChoosed);
+        playSound(userChoosed);
+        animatePress(userChoosed);
+        checkAnswer(userPattern.length-1);
+    }
+);
+
+
+function checkAnswer(currentlevel){
+    if(userPattern[currentlevel]===gamePattern[currentlevel]){
+    if(userPattern.length === gamePattern.length){
+        setTimeout(function(){
+            sequence();
+        },1000)
+    }
+}
+    else{
+        playSound('wrong');
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+          }, 200);
+          startOver();
+    }
+}
+
+
+function animatePress(color){
+    $('#'+color).addClass('pressed');
+    setTimeout(function(){
+        $('#'+color).removeClass('pressed');
+    },100);
+  
+}
+
+function sequence(){
+    userPattern=[];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var generatedPattern = color[randomNumber];
+    gamePattern.push(generatedPattern);
+    $('#'+ generatedPattern).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(generatedPattern);
+
+}
+
+
+
+function playSound(name){
+    var audio = new Audio('sounds/'+name+'.mp3');
+    audio.play();
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    start = false;
+  }
